8abbcb8a2cdfb14c990564f4bf4319342cfdf06d
Junichi Ito <jit@sonicgarden.jp>
2020-07-17 20:23:19 +0900

    Refine spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,7 @@
 FactoryBot.define do
-  factory :user, class: User do
-    username {"test_user"}
-    email {"test_user@mail.com"}
-    password {"password"}
-    password_confirmation {"password"}
-  end
-  factory :existed_user, class: User do
-    username { "existed_user" }
-    email { "existed_user@email.com" }
+  factory :user do
+    username { "test_user" }
+    sequence(:email) { |n| "test_user_#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject(:user) { FactoryBot.build(:user, email: email) }
-
-  context "emailが重複してする場合" do
-    existed_user =  User.find_by(username: "existed_user") || FactoryBot.create(:existed_user)
-    let(:email) { existed_user.email }
+  context "emailが重複する場合" do
+    let!(:existing_user) { FactoryBot.create(:user, email: 'alice@example.com') }
+    subject { FactoryBot.build(:user, email: 'alice@example.com') }
     it { is_expected.to be_invalid }
   end
 end
