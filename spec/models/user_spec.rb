936fea71f91fa696db1923d17958fcac0e23e710
Junichi Ito <jit@sonicgarden.jp>
2020-07-17 20:18:00 +0900

    Write spec per https://qiita.com/daddy_hacker/items/c84ae1b65831d5f7264d

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { FactoryBot.build(:user, email: email) }
+
+  context "emailが重複してする場合" do
+    existed_user =  User.find_by(username: "existed_user") || FactoryBot.create(:existed_user)
+    let(:email) { existed_user.email }
+    it { is_expected.to be_invalid }
+  end
 end
